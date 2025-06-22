4ff1c12b7be5970d4dbf75d78b8dca9bc61ea918
Sergei9930 <psenicnyj806@gmail.com>
2025-06-23 00:48:53 +0300

    Fix DumpPump import and initialization

---
+++ Management/DumpPump/DumpPump.js
@@ -0,0 +1,19 @@
+function handleDumpPump() {
+  // TODO: реализовать логику обработчика
+}
+
+function processMessage(msg) {
+  // TODO: реализовать обработку сообщения
+}
+
+let botInstance;
+
+function setBot(bot) {
+  botInstance = bot;
+}
+
+module.exports = {
+  handleDumpPump,
+  processMessage,
+  setBot
+};
